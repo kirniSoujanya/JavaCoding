2a94e25f4944a0398f7aa188f13ca2ecee064f3e
Soujanya <soujanyakirni99@gmail.com>
2024-05-18 14:55:27 -0700

    Program1
    HelloWorld Program

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package kickstart;
+
+/**
+ * Author Soujanya Kirni 05-18-2024
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("Hello World " + i);
+		}
+	}
+
+}
